ed48329494c16365350b4e969f8c56bd05435139
CamiloGato <cm.andres022@hotmail.com>
2024-12-30 09:59:05 -0500

    Update: Creation of awaitable and graphic methods

---
+++ Assets/Tools/Extension/AwaitableExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Tools.Extension
+{
+    public static class AwaitableExtensions
+    {
+        public static async void OnComplete(this Awaitable awaitable, System.Action onComplete)
+        {
+            try
+            {
+                await awaitable;
+                onComplete?.Invoke();
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e);
+            }
+        }
+    }
+}
---
+++ Assets/Tools/Extension/GraphicExtensions.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Tools.Extension
+{
+    public static class GraphicExtensions
+    {
+        public static async Awaitable CrossFadeAlphaAsync(this Graphic graphic, float alpha, float duration, bool ignoreTimeScale = false)
+        {
+            graphic.CrossFadeAlpha(alpha, duration, ignoreTimeScale);
+            await Awaitable.WaitForSecondsAsync(duration);
+        }
+
+        public static async Awaitable CrossFadeColorAsync(this Graphic graphic, Color targetColor, float duration, bool ignoreTimeScale = false, bool useAlpha = true)
+        {
+            graphic.CrossFadeColor(targetColor, duration, ignoreTimeScale, useAlpha);
+            await Awaitable.WaitForSecondsAsync(duration);
+        }
+
+    }
+}
